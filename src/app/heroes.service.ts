c949a52ed137c1548f09bdf9c5b0ec5977443c32
francisco <feprieto1@gmail.com>
2021-12-10 00:01:54 -0300

    actualizan2

--- src/app/heroes.service.ts
+++ src/app/heroes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { Heroe } from './classes/heroe';
 
 @Injectable()
